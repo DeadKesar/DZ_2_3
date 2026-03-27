caafd2c771b9038ca3f86dc30ec1a43730f1a95a
MasterChief-96 <m1kmine@ya.ru>
2026-03-27 11:48:45 +0300

    ocpcase2ispravlenie Салгереев ИДБ-24-17
    проблема была в том, что ареа калкулатор нарушает OCP: при добавлении новой фигуры придется менять калкулейт добавляя новый иф элс
    
    
    теперь ареакалкулатор больше не знает о конкретных типах серкл, ректангл;
    для новой фигуры достаточно:
    создать новый класс
    реализовать айшейп
    ареакалкулатор менять не нужно

--- ConsoleApp2/ocp/Case2.cs
+++ ConsoleApp2/ocp/Case2.cs
@@ -1,110 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2.ocp
 {
-    class Case2
+    interface IShape
     {
-        class Circle
+        double GetArea();
+    }
+
+    class Circle : IShape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
         {
-            public double Radius;
-            public Circle(double radius)
-            {
-                Radius = radius;
-            }
-            public double GetArea()
-            {
-                return Math.PI * Radius * Radius;
-            }
-        }
-        class Rectangle
-        {
-            public double Width;
-            public double Height;
-            public Rectangle(double width, double height)
-            {
-                Width = width;
-                Height = height;
-            }
-            public double GetArea()
-            {
-                return Width * Height;
-            }
-        }
-        class AreaCalculator
-        {
-            public double Calculate(object shape)
-            {
-                if (shape is Circle)
-                {
-                    return ((Circle)shape).GetArea();
-                }
-                else if (shape is Rectangle)
-                {
-                    return ((Rectangle)shape).GetArea();
-                }
-                return 0;
-            }
-        }
-        class Program
-        {
-            static void Main()
-            {
-                Circle c = new Circle(5);
-                Rectangle r = new Rectangle(4, 6);
-                AreaCalculator calculator = new AreaCalculator();
-                Console.WriteLine("Circle area: " + calculator.Calculate(c));
-                Console.WriteLine("Rectangle area: " + calculator.Calculate(r));
-            }
+            Radius = radius;
         }
 
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
 
-        //class Figure
-        //{
-        //    public virtual double GetArea()
-        //    {
-        //        return 0;
-        //    }
-        //}
+    class Rectangle : IShape
+    {
+        public double Width { get; }
+        public double Height { get; }
 
-        //class Circle : Figure
-        //{
-        //    public double Radius { get; set; }
-        //    public Circle(double radius)
-        //    {
-        //        Radius = radius;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Math.PI * Radius * Radius;
-        //    }
-        //}
-        //class Rectangle : Figure
-        //{
-        //    public double Width { get; set; }
-        //    public double Height { get; set; }
-        //    public Rectangle(double width, double height)
-        //    {
-        //        Width = width;
-        //        Height = height;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Width * Height;
-        //    }
-        //}
-        //class Application
-        //{
-        //    static void Main()
-        //    {
-        //        var c = new Circle(5);
-        //        var r = new Rectangle(4, 6);
-        //        Console.WriteLine("Circle area: " + c.GetArea());
-        //        Console.WriteLine("Rectangle area: " + r.GetArea());
-        //    }
-        //}
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class AreaCalculator
+    {
+        public double Calculate(IShape shape)
+        {
+            return shape.GetArea();
+        }
+
+        public double CalculateTotal(IEnumerable<IShape> shapes)
+        {
+            double total = 0;
+
+            foreach (var shape in shapes)
+            {
+                total += shape.GetArea();
+            }
+
+            return total;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IShape c = new Circle(5);
+            IShape r = new Rectangle(4, 6);
+
+            AreaCalculator calculator = new AreaCalculator();
+
+            Console.WriteLine("Circle area: " + calculator.Calculate(c));
+            Console.WriteLine("Rectangle area: " + calculator.Calculate(r));
+
+            var shapes = new List<IShape> { c, r };
+            Console.WriteLine("Total area: " + calculator.CalculateTotal(shapes));
+        }
     }
 }
