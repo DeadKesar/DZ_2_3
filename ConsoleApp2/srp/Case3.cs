d0959b5646c2b1f406923b54f025b36d071eacfa
ElenaMatulova <alenaalesina057@gmail.com>
2026-03-27 15:56:35 +0300

    Update Case3.cs

--- ConsoleApp2/srp/Case3.cs
+++ ConsoleApp2/srp/Case3.cs
@@ -8,68 +8,45 @@ namespace ConsoleApp2.srp
 {
     class Case3
     {
-        public class Order //этот класс будет отвечать только за хранение данных заказа, добавлен public для доступа 
+        class Order
         {
-            public int OrderId ;
+            public int OrderId;
             public List<string> Items = new List<string>();
 
             public void AddItem(string item)
             {
                 Items.Add(item);
             }
-        }
-        
-        public class OrderRepository // этот класс отвечает только за хранение закозов в базе данных
-        {
-            public void Save(Order order) //сокращение имени функции для читаемости - отсутствие загромождённости. +добавление параметра
+
+            public void SaveToDatabase()
             {
-                if (order == null)    //проверка на пустой заказ
-                    throw new ArgumentNullException(nameof(order));    //nameof(order) - защита от переименования
                 Console.WriteLine("Order saved to database!");
             }
-        }
 
-        public class OrderPrinter //этот класс отвечает только за вывод данных заказа
-        {
-            public void Print(Order order) //сокращение имени функции для читаемости - отсутствие загромождённости +добавление параметра
+            public void PrintOrder()
             {
-                if (order == null)    //проверка на пустой заказ
-                    throw new ArgumentNullException(nameof(order));
-                Console.WriteLine("Order #" + order.OrderId); // order.OrderId - тк OrderId теперь в другом классе
-                
-                foreach (var item in order.Items)    //order.Items - Items теперь в другом классе
+                Console.WriteLine("Order #" + OrderId);
+                foreach (var item in Items)
                 {
                     Console.WriteLine(" - " + item);
                 }
             }
-        }
 
-        public class EmailNotification    //этот класс отвечает за отправку email
-        {
-            public void Send(Order order)    //сокращение имени функции для читаемости - отсутствие загромождённости +добавление параметра
+            public void SendOrderConfirmation()
             {
-                if (order == null)    //проверка на пустой заказ
-                    throw new ArgumentNullException(nameof(order));
                 Console.WriteLine("Order confirmation email sent!");
             }
         }
-
 
         public class App
         {
             static void Main()
             {
                 Order order = new Order();
-                order.OrderId = 1;     //добавляем id к заказу
                 order.AddItem("Laptop");
-                
-                OrderPrinter printer = new OrderPrinter();    //создание объектов новых классов
-                OrderRepository repository = new OrderRepository();
-                EmailNotification emailService = new EmailNotification();
-                
-                printer.Print(order);    // использование самих функций
-                repository.Save(order);
-                emailService.Send(order);
+                order.PrintOrder();
+                order.SaveToDatabase();
+                order.SendOrderConfirmation();
             }
         }
     }
