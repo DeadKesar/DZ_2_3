61ea90e720464a8e2cc0a1f62765873e8eca28e1
viktoria1211mart-coder <viktoria1211mart@gmail.com>
2026-03-28 01:26:32 +0300

    Update Case3.cs

--- ConsoleApp2/isp/Case3.cs
+++ ConsoleApp2/isp/Case3.cs
@@ -1,23 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ConsoleApp2.isp
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тестирование StandardFile");
+            var standardFile = new Case3.StandardFile("отчет.txt", "C:/docs");
+            standardFile.OpenFile();
+            standardFile.WriteFile("Новые данные");
+            standardFile.ShareFile("admin@example.com");
+
+            Console.WriteLine("\nТестирование ReadOnlyFile");
+            var readOnlyFile = new Case3.ReadOnlyFile("конфиг.sys", "C:/system");
+            readOnlyFile.OpenFile();
+            Console.WriteLine(readOnlyFile.ReadFile());
+            Console.WriteLine("\nНажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+
     internal class Case3
     {
-        public interface IFileRoleOperations
-        {
-            void OpenFile();
-            string ReadFile();
-            void WriteFile(string content);
-            void ShareFile(string recipient);
-            void ArchiveFile();
-        }
+        public interface IReadable { void OpenFile(); string ReadFile(); }
+        public interface IWritable { void WriteFile(string content); }
+        public interface IShareable { void ShareFile(string recipient); }
+        public interface IArchivable { void ArchiveFile(); }
 
-        public class StandardFile : IFileRoleOperations
+        public class StandardFile : IReadable, IWritable, IShareable, IArchivable
         {
             public string FileName { get; set; }
             public string FilePath { get; set; }
@@ -28,38 +39,14 @@ namespace ConsoleApp2.isp
                 FilePath = filePath;
             }
 
-            public void OpenFile()
-            {
-                Console.WriteLine("Opening file " + FileName + " at " + FilePath);
-            }
-
-            public string ReadFile()
-            {
-                return "Contents of " + FileName;
-            }
-
-            public void WriteFile(string content)
-            {
-                Console.WriteLine("Writing to file " + FileName + ": " + content);
-            }
-
-            public void ShareFile(string recipient)
-            {
-                Console.WriteLine("Sharing file " + FileName + " with " + recipient);
-            }
-
-            public void ArchiveFile()
-            {
-                Console.WriteLine("Archiving file " + FileName);
-            }
-
-            public void GetFileDetails()
-            {
-                Console.WriteLine("File details: " + FileName + ", located at " + FilePath);
-            }
+            public void OpenFile() => Console.WriteLine($"Открытие: {FileName}");
+            public string ReadFile() => $"Чтение данных из {FileName}";
+            public void WriteFile(string content) => Console.WriteLine($"Запись в {FileName}: {content}");
+            public void ShareFile(string recipient) => Console.WriteLine($"Файл {FileName} отправлен {recipient}");
+            public void ArchiveFile() => Console.WriteLine($"Файл {FileName} архивирован");
         }
 
-        public class ReadOnlyFile : IFileRoleOperations
+        public class ReadOnlyFile : IReadable
         {
             public string FileName { get; set; }
             public string FilePath { get; set; }
@@ -70,36 +57,8 @@ namespace ConsoleApp2.isp
                 FilePath = filePath;
             }
 
-            public void OpenFile()
-            {
-                Console.WriteLine("Opening read-only file " + FileName + " at " + FilePath);
-            }
-
-            public string ReadFile()
-            {
-                return "Read-only content from " + FileName;
-            }
-
-            public void WriteFile(string content)
-            {
-                throw new NotSupportedException("Cannot write to a read-only file " + FileName);
-            }
-
-            public void ShareFile(string recipient)
-            {
-                throw new NotSupportedException("Sharing is not supported for read-only file " + FileName);
-            }
-
-            public void ArchiveFile()
-            {
-                throw new NotSupportedException("Archiving is not supported for read-only file " + FileName);
-            }
-
-            public void GetFileInfo()
-            {
-                Console.WriteLine("File Info: " + FileName + " at " + FilePath);
-            }
+            public void OpenFile() => Console.WriteLine($"Открытие (только чтение): {FileName}");
+            public string ReadFile() => $"Данные из защищенного файла {FileName}";
         }
-
     }
 }
