e32662aa8696445a7f8f2bc77651f106ecd26a63
Alexander Schaefer <135496460+mcwkrsk@users.noreply.github.com>
2026-03-28 03:54:03 +0300

    ИДБ-24-17 Шефер Александр: SRP Case 2
    Что сделал 
    
    1. Разделение ответственности
    Исходный класс User содержал в себе всё: хранение данных, регистрацию, смену пароля, печать информации, отправку email. Это нарушало принцип единственной ответственности.
    
    Выделил отдельные классы для каждой зоны ответственности:
    
    User – только хранение данных (поля Name, Email, Password).
    UserRegistrationService – отвечает за регистрацию и смену пароля.
    UserPrinter – отвечает за вывод информации о пользователе.
    EmailService – отвечает за отправку email-сообщений.
    
    2. Добавление конструктора в User
    Чтобы гарантировать, что пользователь создаётся с обязательными полями, добавил конструктор User(string name, string email, string password).
    
    3. Очистка кода 
    Удалил из класса User методы Register, PrintUserInfo, ChangePassword, SendEmail, которые были перенесены в соответствующие сервисы.
    
    В классе App изменил логику: теперь он создаёт необходимые сервисы и использует их для выполнения операций (регистрация, печать, отправка email), а не вызывает методы напрямую у объекта User.

--- ConsoleApp2/srp/Case2.cs
+++ ConsoleApp2/srp/Case2.cs
@@ -8,34 +8,53 @@ namespace ConsoleApp2.srp
 {
     class Case2
     {
-        class User
+        // Унарный акк
+        public class User
         {
-            public string Name;
-            public string Email;
-            public string Password;
+            public string Name { get; set; } // Поставил публичный сет, так как это просто класс-хранилище
+            public string Email { get; set; }
+            public string Password { get; set; }
 
-            public void Register(string name, string email, string password)
+            public User(string name, string email, string password)
             {
                 Name = name;
                 Email = email;
                 Password = password;
+            }
+        }
+
+        // Класс регистрационной службы
+        public class UserRegistrationService
+        {
+            public User Register(string name, string email, string password)
+            {
+                var user = new User(name, email, password);
                 Console.WriteLine("User registered!");
+                return user;
             }
 
-            public void PrintUserInfo()
+            public void ChangePassword(User user, string newPassword)
             {
-                Console.WriteLine("User: " + Name + " Email: " + Email);
-            }
-
-            public void ChangePassword(string newPassword)
-            {
-                Password = newPassword;
+                user.Password = newPassword;
                 Console.WriteLine("Password changed!");
             }
+        }
 
-            public void SendEmail(string message)
+        // Класс-справка
+        public class UserPrinter
+        {
+            public void Print(User user)
             {
-                Console.WriteLine("Email sent to " + Email + ": " + message);
+                Console.WriteLine("User: " + user.Name + " Email: " + user.Email);
+            }
+        }
+
+        // Класс для почты
+        public class EmailService
+        {
+            public void SendEmail(string email, string message)
+            {
+                Console.WriteLine("Email sent to " + email + ": " + message);
             }
         }
 
@@ -43,10 +62,14 @@ namespace ConsoleApp2.srp
         {
             public void Execute()
             {
-                User user = new User();
-                user.Register("Tim", "tim@example.com", "123456");
-                user.PrintUserInfo();
-                user.SendEmail("Hello!");
+                var registrationService = new UserRegistrationService();
+                var user = registrationService.Register("Tim", "tim@example.com", "123456");
+
+                var printer = new UserPrinter();
+                printer.Print(user);
+
+                var emailService = new EmailService();
+                emailService.SendEmail(user.Email, "Hello!");
             }
         }
     }
