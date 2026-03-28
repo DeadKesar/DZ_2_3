d21458154987dbe3850d753c1bf041fd48bf719d
Alexander Schaefer <135496460+mcwkrsk@users.noreply.github.com>
2026-03-28 19:30:42 +0300

    ИДБ-24-17 Шефер Александр: OCP Case 1
    Что сделал (OCP, Case1)
    1. Устранение нарушения открытости/закрытости (OCP)
    Исходный класс User содержал проверку if (this is ICoolGuy) внутри метода DrawUser. Это нарушало принцип OCP, так как при добавлении нового типа пользователя (например, ФСБ) пришлось бы изменять существующий код.
    
    Заменил условную логику на полиморфизм:
    Сделал класс User абстрактным.
    Выделил общую логику отрисовки в виртуальный метод DrawUser.
    Создал два конкретных класса:
    
    RegularUser – обычный пользователь, использует только базовую отрисовку.
    CoolUser – наследник User, реализующий интерфейс ICoolGuy. Переопределяет DrawUser, вызывая базовый метод и добавляя отрисовку очков.
    
    2. Улучшение архитектуры
    Ввёл интерфейс ICoolGuy как контракт для “крутых” пользователей.
    
    Теперь каждый тип пользователя сам определяет свою логику отрисовки, а не проверяется через is.
    
    3. Очистка кода (Clean Code)
    
    Добавил осмысленные сообщения в Console.WriteLine для наглядности.
    Привёл имена методов и полей к единому стилю.
    
    4. Инкапсуляция
    Поля _isSelected и _image остались protected, чтобы наследники могли к ним обращаться, но не могли изменять извне.
    Конструктор базового класса гарантирует корректную инициализацию.
    
    В итоге:
    Код стал открытым для расширения (новые типы пользователей можно добавлять, не трогая существующие классы) и закрытым для модификации. Принцип OCP соблюдён.

--- ConsoleApp2/ocp/Case1.cs
+++ ConsoleApp2/ocp/Case1.cs
@@ -1,40 +1,79 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp2.ocp
 {
     class Case1
     {
-        interface ICoolGuy
+        // Контракт крутого парня
+        public interface ICoolGuy
         {
-            void CallCoolGuy();
+            void DrawCoolGuyGlasses();
         }
-        class User
-        {
-            private bool _isSelected;
-            private string _image;
 
+        // Абстракция в виде класса - пользователя
+        public abstract class User
+        {
+            protected bool _isSelected;
+            protected string _image;
+
+            // Конструктор
             public User(bool isSelected, string image)
             {
                 _isSelected = isSelected;
                 _image = image;
             }
-            public void DrawUser()
+
+            // Метод для рисования пользователя
+            public virtual void DrawUser()
             {
                 if (_isSelected)
                     DrawEllipseAroundUser();
                 if (_image != null)
                     DrawImageOfUser();
-                if (this is ICoolGuy) // редкий случай
-                    DrawCoolGuyGlasses();
-                // И т. д.
             }
-            void DrawEllipseAroundUser() { }
-            void DrawImageOfUser() { }
-            void DrawCoolGuyGlasses() { }
+
+            // Добавил хоть что-то в эти методы без кода
+            // Метод для рисования контура вокруг пользователя
+            protected virtual void DrawEllipseAroundUser()
+            {
+                Console.WriteLine("Drawing ellipse around user");
+            }
+
+            // Метод для рисования изображения пользователя 
+            protected virtual void DrawImageOfUser()
+            {
+                Console.WriteLine("Drawing user image");
+            }
+        }
+
+        // Класс обычного пользователя
+        public class RegularUser : User
+        {
+            public RegularUser(bool isSelected, string image) : base(isSelected, image) { }
+        }
+
+        // Класс "крутого" пользователя
+        public class CoolUser : User, ICoolGuy
+        {
+            public CoolUser(bool isSelected, string image) : base(isSelected, image) { }
+
+            // Метод для рисования очков
+            public void DrawCoolGuyGlasses()
+            {
+                Console.WriteLine("Drawing cool guy glasses");
+            }
+
+            // Метод для рисования "крутого" пользователя
+            public override void DrawUser()
+            {
+                base.DrawUser();
+                DrawCoolGuyGlasses();
+            }
         }
     }
 }
