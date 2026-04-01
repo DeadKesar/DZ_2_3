ed24ecc56ebfe7058a28d90864dbae0ca56b1f44
ApexFortress <kralex06@mail.ru>
2026-04-01 17:59:30 +0300

    srp Case2.cs
    В целом через наследование можно распределить данные по разным классам, что я и сделал

--- ConsoleApp2/srp/Case2.cs
+++ ConsoleApp2/srp/Case2.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// srp принцип: один класс - одна задача
 namespace ConsoleApp2.srp
 {
     class Case2
     {
-        class User
+        class User //Только хранение данных
         {
-            public string Name;
-            public string Email;
-            public string Password;
-
-            public void Register(string name, string email, string password)
+            protected string Name;
+            protected string Email;
+            protected string Password;
+        }
+          class Reg: User{  //Из данных юзера только регистрация 
+              public void Register(string name, string email, string password)
             {
                 Name = name;
                 Email = email;
                 Password = password;
                 Console.WriteLine("User registered!");
             }
-
+                   }
+        class PrintUserinf:Reg {// Только вывод данных из регистрации
             public void PrintUserInfo()
             {
                 Console.WriteLine("User: " + Name + " Email: " + Email);
             }
-
+        }
+        class MyPassword{// Только именение пароля
             public void ChangePassword(string newPassword)
             {
                 Password = newPassword;
                 Console.WriteLine("Password changed!");
             }
-
+        }
+        class Mailsend:Reg{// Только отправка сообщения
             public void SendEmail(string message)
             {
                 Console.WriteLine("Email sent to " + Email + ": " + message);
@@ -43,10 +47,10 @@ namespace ConsoleApp2.srp
         {
             public void Execute()
             {
-                User user = new User();
-                user.Register("Tim", "tim@example.com", "123456");
-                user.PrintUserInfo();
-                user.SendEmail("Hello!");
+                User user = new User();// для корректной работы 
+                Reg.Register("Tim", "tim@example.com", "123456"); // смена класса
+                PrintUserinf.PrintUserInfo();//смена класаа
+                Mailsend.SendEmail("Hello!");// смена класса
             }
         }
     }
