d4543f8eb3d0f804e025d8d181a36f85f943f150
Arseniy Harin <125690009+HWOarsHAR@users.noreply.github.com>
2026-04-04 22:44:56 +0300

    Update Case2.cs
    Добавил 2 доп. класса для делегирования задач регистрации и отправки писем. До этого класс User был God Object'ом, что не соответствует принципам SOLID, в частности SRP - принцип простой ответственности.

--- ConsoleApp2/srp/Case2.cs
+++ ConsoleApp2/srp/Case2.cs
@@ -1,41 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection.Configuration;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp2.srp
 {
     class Case2
     {
+        // До этого класс User был god object. Теперь у нас разделены обязанности.
+
+        // 1-ый класс для хранения
+        // 2-ой для регистрации пользователя
+        // 3-ий для имэйлов
+
+        // Класс для хранения встроенных(первоначальных) типов данных пользователя (int, string, bool и т.д.)
         class User
         {
             public string Name;
             public string Email;
             public string Password;
 
-            public void Register(string name, string email, string password)
+            public User(string name, string email, string password)
             {
                 Name = name;
                 Email = email;
                 Password = password;
-                Console.WriteLine("User registered!");
             }
-
             public void PrintUserInfo()
             {
                 Console.WriteLine("User: " + Name + " Email: " + Email);
             }
+        }
 
-            public void ChangePassword(string newPassword)
+        // Класс для регистрации пользователя
+        class UserHandler
+        {
+            public User Register(string name, string email, string password)
             {
-                Password = newPassword;
+                User user = new User(name, email, password);
+                Console.WriteLine("User registered!");
+                return user;
+            }
+
+            public void ChangePassword(User user, string new_password)
+            {
+                user.Password = new_password;
                 Console.WriteLine("Password changed!");
             }
 
-            public void SendEmail(string message)
+        }
+
+        // Класс для работы с имэйлами
+        class EmailHandler
+        {
+            public void SendEmail(User user, string message)
             {
-                Console.WriteLine("Email sent to " + Email + ": " + message);
+                Console.WriteLine("Email sent to " + user.Email + ": " + message);
             }
         }
 
@@ -43,10 +66,12 @@ namespace ConsoleApp2.srp
         {
             public void Execute()
             {
-                User user = new User();
-                user.Register("Tim", "tim@example.com", "123456");
+                UserHandler userhandler = new UserHandler();
+                EmailHandler emailhandler = new EmailHandler();
+
+                User user = userhandler.Register("Arseniy Kharin", "trololololololil@kester.ru", "adji_kester123");
                 user.PrintUserInfo();
-                user.SendEmail("Hello!");
+                emailhandler.SendEmail(user, "Hello, Kester! I wan't to enter the Military Training Center");
             }
         }
     }
