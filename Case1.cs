3f9894977a809162154ebcfadcd73bc987e0ea24
nyustazer <nyustazer@gmail.com>
2026-04-05 19:29:58 +0300

    Add files via upload

---
+++ Case1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.ocp
+{
+    class Case1
+    {
+        interface ICoolGuy
+        {
+            void CallCoolGuy();
+        }
+
+        class User
+        {
+            private bool _isSelected;
+            private string _image;
+
+            public User(bool isSelected, string image)
+            {
+                _isSelected = isSelected;
+                _image = image;
+            }
+
+            public virtual void DrawUser()//стал virtual 
+            {
+                if (_isSelected)
+                    DrawEllipseAroundUser();
+
+                if (_image != null)
+                    DrawImageOfUser();
+            }
+
+            private void DrawEllipseAroundUser() { }
+            private void DrawImageOfUser() { }
+            protected void DrawCoolGuyGlasses() { }//protected для наследников + clean code
+        }
+
+        // новый класс для расширения через наследование и переопределение
+        class CoolGuyUser : User
+        {
+            public CoolGuyUser(bool isSelected, string image)
+                : base(isSelected, image)
+            {
+            }
+
+            public override void DrawUser()
+            {
+                base.DrawUser();
+                DrawCoolGuyGlasses();      //специфичное поведение
+            }
+        }
+    }
+}
---
+++ Case2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp2.ocp
+{
+    class Case2
+    {
+        // Абстракция для всех фигур, AreaCalculator закрыт для модификации, PUBLIC для использования вне case2, если надо
+        public interface IShape
+        {
+            double GetArea();
+        }
+
+        public class Circle : IShape
+        {
+            public double Radius { get; }//clean code
+
+            public Circle(double radius)
+            {
+                Radius = radius;
+            }
+
+            public double GetArea()
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+
+        public class Rectangle : IShape
+        {
+            public double Width { get; }//clean code
+            public double Height { get; }//clean code
+
+            public Rectangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public double GetArea()
+            {
+                return Width * Height;
+            }
+        }
+
+        public class AreaCalculator
+        {
+            // Теперь метод принимает абстракцию IShape
+            public double Calculate(IShape shape)
+            {
+                return shape.GetArea();//Нет if-ов OCP принцип
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                var circle = new Circle(5);
+                var rectangle = new Rectangle(4, 6);
+
+                var calculator = new AreaCalculator();
+
+                Console.WriteLine("Circle area: " + calculator.Calculate(circle));
+                Console.WriteLine("Rectangle area: " + calculator.Calculate(rectangle));
+            }
+        }
+    }
+}
