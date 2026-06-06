2b0cd7250cc50310f4db5fd9a8639889095400bd
Maxim <121259221+icava@users.noreply.github.com>
2026-06-07 02:03:25 +0300

    Update Case2.cs
    Выполнил ДЗ по LSP (Кейс3)
    Что исправил:
    Там класс FrozenAccount наследовался от BankAccount, но ломал логику базового класса. Метод Withdraw в нем был пустой и ничего не делал, а Deposit просто выводил текст в консоль без изменения баланса. Это нарушало принцип Лисков, потому что заблокированный аккаунт вел себя совсем не так, как обычный, и код мог сломаться при вызове этих методов.
    
    Как сделал:
    Удалил класс FrozenAccount. Вместо наследования просто добавил булевую переменную IsFrozen в сам класс BankAccount, а также методы Freeze() и Unfreeze() для смены состояния. Теперь внутри Deposit и Withdraw стоит обычная проверка через if — если счет заморожен, то выбрасывается исключение Exception. Ну и строки переписал через интерполяцию $, чтобы читалось лучше.

--- ConsoleApp2/lsp/Case2.cs
+++ ConsoleApp2/lsp/Case2.cs
@@ -12,61 +12,53 @@ namespace ConsoleApp2.lsp
         {
             public string AccountNumber { get; set; } = Guid.NewGuid().ToString();
             public double Balance { get; set; }
+            public bool IsFrozen { get; set; } = false;
 
             public virtual void Deposit(double amount)
             {
+                if (IsFrozen)
+                {
+                    throw new Exception("Account is frozen");
+                }
+
                 Balance += amount;
-                Console.WriteLine("Deposited " + amount + " into account " + AccountNumber);
+                Console.WriteLine($"Deposited {amount} into account {AccountNumber}");
             }
 
             public virtual void Withdraw(double amount)
             {
+                if (IsFrozen)
+                {
+                    throw new Exception("Account is frozen");
+                }
+
                 Balance -= amount;
-                Console.WriteLine("Withdrew " + amount + " from account " + AccountNumber);
+                Console.WriteLine($"Withdrew {amount} from account {AccountNumber}");
             }
 
             public virtual void Transfer(BankAccount targetAccount, double amount)
             {
                 Withdraw(amount);
                 targetAccount.Deposit(amount);
-                Console.WriteLine("Transferred " + amount + " from account " + AccountNumber + " to " + targetAccount.AccountNumber);
+                Console.WriteLine($"Transferred {amount} from account {AccountNumber} to {targetAccount.AccountNumber}");
             }
 
             public virtual string GetAccountInfo()
             {
-                return "Account: " + AccountNumber + " with balance: " + Balance;
-            }
-
-            public virtual void UpdateAccountDetails()
-            {
-                Console.WriteLine("Updating account details for " + AccountNumber);
-            }
-        }
-
-        public class FrozenAccount : BankAccount
-        {
-            public bool IsFrozen { get; set; } = true;
-
-            public override void Withdraw(double amount)
-            { }
-
-            public override void Deposit(double amount)
-            {
-                Console.WriteLine("Cannot deposit to a frozen account " + AccountNumber);
-            }
-
-            public void Unfreeze()
-            {
-                IsFrozen = false;
-                Console.WriteLine("Account " + AccountNumber + " is now unfrozen");
+                return $"Account: {AccountNumber} with balance: {Balance}";
             }
 
             public void Freeze()
             {
                 IsFrozen = true;
-                Console.WriteLine("Account " + AccountNumber + " is frozen again");
+                Console.WriteLine($"Account {AccountNumber} is frozen");
+            }
+
+            public void Unfreeze()
+            {
+                IsFrozen = false;
+                Console.WriteLine($"Account {AccountNumber} is now unfrozen");
             }
         }
-
     }
 }
