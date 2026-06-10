0cdb31e99ea46580ec83fdd010afbc9d85a24be4
sergegod-maxx <zxcavenger@gmail.com>
2026-06-10 19:04:58 +0300

    Update Case1.cs

--- ConsoleApp2/ocp/Case1.cs
+++ ConsoleApp2/ocp/Case1.cs
@@ -8,33 +8,57 @@ namespace ConsoleApp2.ocp
 {
     class Case1
     {
-        interface ICoolGuy
+        public interface IUser
         {
-            void CallCoolGuy();
+            void Draw();
         }
-        class User
+
+        public abstract class User : IUser
         {
-            private bool _isSelected;
-            private string _image;
+            protected bool _isSelected;
+            protected string _image;
 
             public User(bool isSelected, string image)
             {
                 _isSelected = isSelected;
                 _image = image;
             }
-            public void DrawUser()
+
+            public virtual void Draw()
             {
                 if (_isSelected)
                     DrawEllipseAroundUser();
                 if (_image != null)
                     DrawImageOfUser();
-                if (this is ICoolGuy) // редкий случай
-                    DrawCoolGuyGlasses();
-                // И т. д.
             }
-            void DrawEllipseAroundUser() { }
-            void DrawImageOfUser() { }
-            void DrawCoolGuyGlasses() { }
+
+            protected void DrawEllipseAroundUser() { }
+            protected void DrawImageOfUser() { }
+        }
+
+        public interface ICoolGuy
+        {
+            void CallCoolGuy();
+        }
+
+        public class CoolGuyUser : User, ICoolGuy
+        {
+            public CoolGuyUser(bool isSelected, string image) : base(isSelected, image) { }
+
+            public void CallCoolGuy() { }
+
+            public override void Draw()
+            {
+                base.Draw();
+                DrawCoolGuyGlasses();
+            }
+
+            private void DrawCoolGuyGlasses() { }
+        }
+
+        public class RegularUser : User
+        {
+            public RegularUser(bool isSelected, string image) : base(isSelected, image) { }
         }
     }
 }
